b912abca23934da20a534ef7f12b91720e6ee515
Ross Ketron <rossketron@gmail.com>
2020-12-18 08:05:22 -0500

    used hook to add active link styling to navbar

--- nextjs-portfolio/components/header/header.js
+++ nextjs-portfolio/components/header/header.js
@@ -1,33 +1,33 @@
 import React from 'react'
-import ActiveLink from '../ActiveLink/ActiveLink';
-import styles from './header.module.css';
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import styles from './header.module.css'
 
-export default function Header() {
+const isActive = (href) => {
+  const router = useRouter()
+  return (router.pathname === href)
+}
+
+const Header = () => {
   return (
     <header className={styles.container}>
       <p className={styles.title}>Portfo<span>lio.</span></p>
       <ul className={styles.page_links}>
-        <li>
-          <ActiveLink href="/">
-            <a>Home</a>
-          </ActiveLink>
+        <li className={ isActive('/') ? styles.active : '' }>
+          <Link href="/"><a>Home</a></Link>
         </li>
-        <li>
-          <ActiveLink href="/aboutme">
-            <a>About</a>
-          </ActiveLink>
+        <li className={ isActive('/aboutme') ? styles.active : '' }>
+          <Link href="/aboutme"><a>About</a></Link>
         </li>
-        <li>
-          <ActiveLink href="/projects">
-            <a>Projects</a>
-          </ActiveLink>
+        <li className={ isActive('/projects') ? styles.active : '' }>
+          <Link href="/projects"><a>Projects</a></Link>
         </li>
-        <li>
-          <ActiveLink href="/contactme">
-            <a>Contact</a>
-          </ActiveLink>
+        <li className={ isActive('/contactme') ? styles.active : '' }>
+          <Link href="/contactme"><a>Contact</a></Link>
         </li>
       </ul>
     </header>
   )
 }
+
+export default Header

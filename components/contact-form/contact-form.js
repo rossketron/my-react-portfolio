f856159020cfa9cf10df028072bba267c72a4276
Ross Ketron <rossketron@gmail.com>
2022-01-15 09:59:23 -0500

    chore: Update styling to use sass

--- components/contact-form/contact-form.js
+++ components/contact-form/contact-form.js
@@ -1,34 +1,57 @@
-import React from 'react'
-import * as emailjs from 'emailjs-com'
-import styles from './contact-form.module.css'
+import React from "react";
+import * as emailjs from "emailjs-com";
+import styles from "./contact-form.module.scss";
 
-const toName = "Ross Ketron"
-const toEmail = "yodaross22@gmail.com"
+const toName = "Ross Ketron";
+const toEmail = "yodaross22@gmail.com";
 
 class ContactForm extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       name: "Name",
       email: "Email",
       subject: "Subject",
-      message: "Message"
-    }
+      message: "Message",
+    };
   }
 
-    render() {
-      return (
-        <div>
-        <form className={styles.form}>      
-          <input name="name" type="text" className={styles.feedbackinput} required placeholder="Name" />   
-          <input name="email" type="text" className={styles.feedbackinput} required placeholder="Email" />
-          <input name="subject" type="text" className={styles.feedbackinput} required placeholder="Subject" />
-          <textarea name="text" className={styles.feedbackinput, styles.textarea} required placeholder="Message"></textarea>
-          <input type="submit" className={styles.submit} value="SUBMIT"/>
+  render() {
+    return (
+      <div>
+        <form className={styles.form}>
+          <input
+            name="name"
+            type="text"
+            className={styles.feedbackinput}
+            required
+            placeholder="Name"
+          />
+          <input
+            name="email"
+            type="text"
+            className={styles.feedbackinput}
+            required
+            placeholder="Email"
+          />
+          <input
+            name="subject"
+            type="text"
+            className={styles.feedbackinput}
+            required
+            placeholder="Subject"
+          />
+          <textarea
+            name="text"
+            className={(styles.feedbackinput, styles.textarea)}
+            required
+            placeholder="Message"
+          ></textarea>
+          <input type="submit" className={styles.submit} value="SUBMIT" />
         </form>
-        </div>
-      )
-    }
+      </div>
+    );
   }
+}
 
-  export default ContactForm;
+export default ContactForm;
--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,7 +1,5 @@
-import styles from './layout.module.css'
+import styles from "./layout.module.scss";
 
 export default function Layout({ children }) {
-  return (
-    <div className={styles.container}>{children}</div>
-  )
+  return <div className={styles.container}>{children}</div>;
 }
